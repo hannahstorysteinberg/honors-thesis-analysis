c1b70ef682db14bc4f3bab63a5808197a7001f5a
hannahstorysteinberg <hannahsteinberg@g.ucla.edu>
2025-03-26 14:39:49 -0700

    Update norming-analysis.R

--- norming-analysis.R
+++ norming-analysis.R
@@ -2,5 +2,10 @@ library(readr)
 library(dplyr)
 df <- read_csv(file = "results.csv",col_names = FALSE)
 head(df)
-colnames(df) <- LETTERS[1:24]
+colnames(df) <- LETTERS[1:24] # like the google sheet
 head(df)
+list_of_names <- as.vector(df[which(df[,"J"] == "name"),"K"])[[1]]
+list_of_names
+length(list_of_names)
+length(unique(list_of_names)) # 1 unique name
+list_of_names[duplicated(list_of_names)]
