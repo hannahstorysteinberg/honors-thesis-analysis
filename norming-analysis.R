907d45c25213cf2a296a12dc761e4d1c1b330e9c
hannahstorysteinberg <hannahsteinberg@g.ucla.edu>
2025-03-28 00:20:38 -0700

    gender!

--- norming-analysis.R
+++ norming-analysis.R
@@ -240,7 +240,25 @@ round(perc_crit_word,2)
 
 # GENDER STUFF
 head(df_gender)
+df_gender_q <- filter(df_gender, Question == "gender")
+df_gender_q$Data <- gsub("Almost definitely a Man", 4,df_gender_q$Data)
+df_gender_q$Data <- gsub("Probably a Man", 3,df_gender_q$Data)
+df_gender_q$Data <- gsub("Not Sure", 2,df_gender_q$Data)
+df_gender_q$Data <- gsub("Probably a Woman", 1,df_gender_q$Data)
+df_gender_q$Data <- gsub("Almost definitely a Woman", 0,df_gender_q$Data)
+df_gender_q$Data <- as.numeric(df_gender_q$Data)
+df_gender_q$sentNum <- as.numeric(gsub("_","",df_gender_q$sentNum))
 
+authors <- sort(unique(df_gender_q$author))
+
+auth <- data.frame(authors = authors, gender = 0) 
+for (i in 1:nrow(auth)) {
+  cur_author <- auth$authors[i]
+  cur_df <- filter(df_gender_q, author == cur_author)
+  auth$gender[i] <- mean(cur_df$Data)
+}
+
+round(auth,2) # larger the number, more male the author's writing is 
 
 # DEMOGRAPHICS
 df_demo <- select(df_demo,c("Name","Phase","TypeOption","Question","TypeChoice","Data"))
