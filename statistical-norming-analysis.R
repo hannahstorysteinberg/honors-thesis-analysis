c27a6a9482700d84deedfe1e89e18d731fe5f0ef
hannahstorysteinberg <hannahsteinberg@g.ucla.edu>
2025-04-09 20:37:41 -0700

    4/9 meeting

--- statistical-norming-analysis.R
+++ statistical-norming-analysis.R
@@ -68,7 +68,7 @@ df_main_qleg <- select(df_main_qleg, c("Name","Phase","Data","isLeg","isSent","s
 
 key_deidentified <- setNames(as.character(1:length(list_of_names)),list_of_names)
 df_main_qleg$Name <- str_replace_all(df_main_qleg$Name,key_deidentified)
-df_main_qleg$Name <- as.numeric(df_main_qleg$Name)
+df_main_qleg$Name <- as.factor(df_main_qleg$Name)
 
 # fix sentNum
 df_main_qleg$sentNum <- as.numeric(gsub("_","",df_main_qleg$sentNum)) # remove the first character with is an underscore
@@ -77,26 +77,74 @@ df_main_qleg$sentNum <- as.numeric(gsub("_","",df_main_qleg$sentNum)) # remove t
 df_main_qleg$Data <- as.numeric(df_main_qleg$Data)
 
 
-df_main_qleg$isLeg <- ifelse(df_main_qleg$isLeg == "leg",0,1)
+df_main_qleg$isLeg <- ifelse(df_main_qleg$isLeg == "leg",1,0)
 
-df_main_qleg$isSent <- ifelse(df_main_qleg$isSent == "sent",0,1)
+df_main_qleg$isSent <- ifelse(df_main_qleg$isSent == "sent",1,0)
+
+# no fillers and no words
+df_main_qleg_no_fill <- filter(df_main_qleg, sentNum < 21)
+
+model_df <- df_main_qleg_no_fill
 
 
 model_legible_data <- lmer(Data ~ 1 + isLeg*isSent
                                     + (1 + isLeg*isSent | sentNum)
                                     + (1 + isLeg*isSent | Name),
-                                    data = df_main_qleg)
+                                    data = df_main_qleg_no_fill)
+
+
 update(model_legible_data, control = lmerControl(calc.derivs = FALSE))
 summary(model_legible_data)
 
+model_df$interaction <- interaction(model_df$isSent, model_df$isLeg)
+
+model_interaction <- lmer(Data ~ 0 + interaction
+                           + (1 + interaction | sentNum)
+                           + (1 + interaction | Name),
+                           data = model_df)
+summary(model_interaction)
+
+# two plots, one for sentences and one for words each with only CRITICAL ITEMS
+
+
+df_main_no_fill <- filter(df_main, isCrit == "crit")
+
+uniqueNames <- unique(df_main_no_fill$fileName)
+isLeg <- ifelse(str_detect(uniqueNames,"leg"),"leg","ill")
+isSent <- ifelse(str_detect(uniqueNames,"word"),"word","sent")
+leg_photo <- data.frame(photo = uniqueNames, isLeg = isLeg, isSent = isSent, legibility = 0)
+for (i in 1:nrow(leg_photo)) { # go through the rows
+  curFileName <- leg_photo$photo[i]
+  leg_photo$legibility[i] <- mean(as.numeric(df_main_no_fill$Data[which(df_main_no_fill$fileName == curFileName & df_main_no_fill$Question == "leg" & !is.na(df_main_no_fill$Data))]))
+}
+leg_photo$legibility <- round(leg_photo$legibility,2)
+leg_photo
+mean(filter(leg_photo,isLeg == "leg")$legibility) # avg legibility of legible sentences
+mean(filter(leg_photo,isLeg == "ill")$legibility) # avg legibility of illegible sentences
+mean(filter(leg_photo,isSent == "word" & isLeg == "ill")$legibility)
+
+
+leg_photo_word <- filter(leg_photo, isSent == "word")
+leg_photo_sent <- filter(leg_photo, isSent == "sent")
+
 # PLOT
-ggplot(leg_photo, aes(x = isLeg, y = legibility, fill = isLeg)) +
+ggplot(leg_photo_word, aes(x = isLeg, y = legibility, fill = isLeg)) +
   stat_summary(fun = mean, geom = "bar", color = "black", width = 0.6, alpha = 0.7) +
   stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) +
   geom_jitter(width = 0.15, alpha = 0.6, size = 2, color = "black") +
   theme_minimal() +
   labs(y = "Average Rating", x = "Legibility") +
   theme(legend.position = "none")
+# continuous legibility???
+
+ggplot(leg_photo_sent, aes(x = isLeg, y = legibility, fill = isLeg)) +
+  stat_summary(fun = mean, geom = "bar", color = "black", width = 0.6, alpha = 0.7) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) +
+  geom_jitter(width = 0.15, alpha = 0.6, size = 2, color = "black") +
+  theme_minimal() +
+  labs(y = "Average Rating", x = "Legibility") +
+  theme(legend.position = "none")
+
 
 
 # DEPENDENT SAMPLES T TEST
