f45f4dd3343969910ad132a70cb1d9d5990e2864
hannahstorysteinberg <hannahsteinberg@g.ucla.edu>
2025-04-15 14:18:49 -0700

    added amanda template

--- classification-test.R
+++ classification-test.R
@@ -1,6 +1,3 @@
-
-test_df <- data.frame(subj = 1:10, sent = 1:10, )
-
 implaus_version <- c("momadoptedadogbutthebabywasupsetbyallthebanks",                
                      "iftheknightseesthebeautyoftheprincesshisheartisgoingtosmell",  
                      "thebuilderknockeddownthewalltofindgoldinfestedwood",           
@@ -41,8 +38,33 @@ plaus_version <- c("momadoptedadogbutthebabywasupsetbyallthebarks",
                    "noahsbakerymakeslemonwafercookiessocrunchyanddelicious",       
                    "stormswerereportednearourcoastsowewereadvisedtostaysafe",      
                    "hetookaimthenthrewthedartstraightatthebullseye",              
-                   "in1390kingrichardiibuiltamoatcirclinghiscity")
+                   "in1390kingrichardbuiltamoatcirclinghiscity")
+test_values <- c(".",                
+                 "iftheknightseesthebeautyoftheprincesshisheartisgoingtoswell",  
+                 "thebuilderknockeddownthewalltofindgoldinfestedwood",           
+                 "acarranontopofaballthatshranktoaflatrounddesk",              
+                 "ifkyleusesthewetmopthenthepileswillbeshiny",                   
+                 "thelumberjacksavedthelogsinhalvtopreparefortheconstruction",   
+                 "theteachlosthergoldringatthecrowdedshoppingmall",            
+                 "emmahitsomefireworksonnewyeareve",                            
+                 "mymomwantsmetodosomanychoresandnagsmeifidontfinishthemquickly",
+                 "alfalfa")
+# some edge cases
+# supposed to be:
+# 1 discard NA
+# 2 non literal 1
+# 3 literal 0
+# 4 menu -> discard NA
+# 5 menu -> choose discard NA
+# 6 menu -> literal 0
+# 7 menu -> non literal 1
+# 8 menu -> literal 0
+# 9 non literal 1
+# 10 discard NA
 
+
+test_df <- data.frame(subj = 1:10, sent = 1:10, value = test_values, text = implaus_version[1:10],leg = 3, sim = 3)
+# test data, leg and sim not important here 
 
 changed_df <- select(test_df, c("subj","sent","value", "text", "leg", "sim"))
 changed_df$isNonLiteral <- NA
@@ -63,17 +85,18 @@ for (i in 1:nrow(changed_df)) {
     
     changed_df$isNonLiteral[i] <- 0 # literal
   }
-  else if (cur_value == "jamilahalfedanonionbecauseusingafullonewasunnecessary") { # special case where halfed is ok
-    changed_df$isNonLiteral[i] <- 1 # nonliteral
-  }
+  # else if (cur_value == "jamilahalfedanonionbecauseusingafullonewasunnecessary") { # special case where halfed is ok
+  #   changed_df$isNonLiteral[i] <- 1 # nonliteral
+  # }
   else if (cur_value == cur_plaus_sent) { # the typed sentence matches the “plausible” version of the sentence, count it as non-literal
     changed_df$isNonLiteral[i] <- 1 # nonliteral
     
-  } else if (!(str_detect(cur_value,cur_implaus_word) | str_detect(cur_value,cur_plaus_word))) { # neither the critical implausible word nor the critical plausible word are found as a substring of the typed string, discard the trial
+  } else if (!(str_detect(cur_value,cur_implaus_word) | str_detect(cur_value,cur_plaus_word) | (str_detect(cur_value, "halfed"))) ) { # neither the critical implausible word nor the critical plausible word are found as a substring of the typed string, discard the trial
+    # plus special case with halfed
     
     rows_to_discard <- c(rows_to_discard, i)
   } else {
-    rows_to_discard <- c(rows_to_discard, i) # just for now discarding ones not sure about, will classify them later
+    #rows_to_discard <- c(rows_to_discard, i) # just for now discarding ones not sure about, will classify them later
     # #counter <- counter + 1, there are 322 ones to evaluate
     holder <- menu(c("literal (implausible)","nonliteral (plausible)", "discard trial"),title = paste("evaluate this sentence: ", cur_value, ". The real implausible sentence is: ", cur_text, ", where the implausible word is ", cur_implaus_word, ", and the plausible word is ", cur_plaus_word, ".", " The difference is ",paste(strsplit(cur_value,"")[[1]][strsplit(cur_value,"")[[1]] != strsplit(cur_text,"")[[1]]], collapse = ""), ".", " The current row number is ", i, " of ", nrow(changed_df), ".", collapse = "", sep = ""))
 
@@ -88,9 +111,9 @@ for (i in 1:nrow(changed_df)) {
       # if answer literal, will answer 1 and -1 is 0
     }
   }
-  print(changed_df$isNonLiteral[i])
-  print(cur_value)
-  print(cur_text)
+  # print(changed_df$isNonLiteral[i])
+  # print(cur_value)
+  # print(cur_text)
 }
 
 rows_to_discard[-1] # which ones to remove
